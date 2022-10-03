6db100661c7494ab789a41e283dfe6fe1fb418f2
Abby <70772021+stuckia@users.noreply.github.com>
2022-10-03 10:28:18 -0400

    Update Prob1_Assignment1.cpp

--- Prob1_Assignment1.cpp
+++ Prob1_Assignment1.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
 #include <math.h>
 
-int main() {
-  int num; // initialize num variable
+int main()
+{
+	//declare variable
+	int num;
 
-  std::cout << "Enter an integer number to see if the given number is "
-               "divisible by 4 or not: "; // retrieve value of num
-  std::cin >> num;
+	//retrieve value and store in num variable
+	std::cout << "Enter an integer number to see if the given number is divisible by 4 or not: ";
+	std::cin >> num;
 
-  // analyze num and print proper results
-  if (num < 0) {
-    std::cout << "The program does not accept negative numbers.";
-  } else if (num <= 100 && num % 4 == 0) {
-    std::cout << "The given number (" << num << ") is divisible by 4."
-              << std::endl;
-    std::cout << "Quotient: " << (num / 4) << " and Remainder: 0";
-  } else if (num <= 100) {
-    std::cout << "The given number (" << num << ") is not divisible by 4."
-              << std::endl;
-    std::cout << "Quotient: " << (num / 4) << " and Remainder: " << (num % 4);
-  } else {
-    std::cout << "The entered number is greater than 100 and square root of "
-                 "the number is "
-              << sqrt(num);
-  }
+	//analyze num
+	if (num < 0)	//check if negative
+	{
+		std::cout << "The program does not accept negative numbers" << std::endl;
+	}
+	else if (num <= 100 && num%4 == 0)		//check if less than or equal to 100 and divisible by 4
+	{
+		std::cout << "The given number (" << num << ") is divisible by 4" << std::endl;
+		std::cout << "Quotient: " << (num / 4) << " and Remainder: 0" << std::endl;
+	}
+	else if (num <= 100)		// check if less than or equal to 100 (catches not divisible by 4)
+	{
+		std::cout << "The given number (" << num << ") is not divisible by 4" << std::endl;
+		std::cout << "Quotient " << (num / 4) << " and Remainder: " << (num % 4) << std::endl;
+	}
+	else	//runs if greater than 100
+	{
+		std::cout << "The entered number is greater than 100 and square root of the number is " << sqrt(num) << std::endl;
+	}
 
-  return 0;
+	//return 0 to end code
+	return 0;
 }
